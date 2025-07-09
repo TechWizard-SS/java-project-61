bb0e6e73b75daad0b1cfc5dfd730fc369e59ff6f
TechWizard-SS <samir898877.s@gmail.com>
2025-07-10 02:36:10 +0400

    third commit

--- app/src/main/java/Hexlet/code/App.java
+++ app/src/main/java/Hexlet/code/App.java
@@ -3,5 +3,8 @@ package Hexlet.code;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        Cli.yourName();
+        System.out.println("Hello, " + Cli.name + "!");
     }
 }
---
+++ app/src/main/java/Hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package Hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+static String name;
+
+    public static String yourName() {
+        Scanner sc = new Scanner(System.in);
+        name = sc.nextLine();
+        return name;
+    }
+}
